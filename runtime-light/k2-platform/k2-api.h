b03073e79da26f6fabb1ce1c7b517e44930b16f6
Vadim Sadokhov <v.sadokhov@vk.team>
2025-05-29 19:11:39 +0300

    wip

--- runtime-light/k2-platform/k2-api.h
+++ runtime-light/k2-platform/k2-api.h
@@ -244,32 +244,36 @@ struct SymbolInfo {
   uint32_t lineno;
 };
 
-inline std::expected<k2::SymbolInfo, int32_t> resolve_symbol(void* addr) noexcept {
+inline k2::SymbolInfo resolve_symbol(void* addr) noexcept {
+  SymbolInfo empty{.name = std::unique_ptr<char, decltype(std::addressof(k2::free))>{nullptr, k2::free},
+                   .filename = std::unique_ptr<char, decltype(std::addressof(k2::free))>{nullptr, k2::free},
+                   .lineno = 0};
+
   size_t name_len{};
   if (auto error_code{k2_symbol_name_len(addr, std::addressof(name_len))}; error_code != k2::errno_ok) [[unlikely]] {
-    return std::unexpected{error_code};
+    return empty;
   }
   size_t filename_len{};
   if (auto error_code{k2_symbol_filename_len(addr, std::addressof(filename_len))}; error_code != k2::errno_ok) [[unlikely]] {
-    return std::unexpected{error_code};
+    return empty;
   }
 
   // +1 since we get non-null-terminated strings from platform and we want to null-terminate them on our side
   auto* name{static_cast<char*>(k2::alloc(name_len + 1))};
   if (name == nullptr) [[unlikely]] {
-    return std::unexpected{k2::errno_enomem};
+    return empty;
   }
 
   auto* filename{static_cast<char*>(k2::alloc(filename_len + 1))};
   if (filename == nullptr) [[unlikely]] {
-    return std::unexpected{k2::errno_enomem};
+    return empty;
   }
 
   ::SymbolInfo symbol_info{.name = name, .filename = filename, .lineno = 0};
   if (auto error_code{k2_resolve_symbol(addr, std::addressof(symbol_info))}; error_code != k2::errno_ok) [[unlikely]] {
     k2::free(filename);
     k2::free(name);
-    return std::unexpected{error_code};
+    return empty;
   }
 
   // null-terminate
